23a0ba9a69c35a162b7bf886c482c891229a0961
ibrarLexumsoft <muhammad.ibrar@lexumsoft.com>
2026-02-24 20:49:12 +0500

    responsive

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,7 +14,7 @@ export default function Home(): JSX.Element {
       <div className="fixed inset-0 z-0 pointer-events-none bg-grid-pattern" />
       <Navbar />
       <main className="relative min-h-screen pt-32 pb-20 px-6">
-        <div className="absolute top-20 left-1/2 -translate-x-1/2 w-[800px] h-[300px] bg-sky-500/5 rounded-full blur-[100px] pointer-events-none" />
+        <div className="absolute top-20 left-1/2 -translate-x-1/2 w-full  md:w-[800px] h-full md:h-[300px] bg-sky-500/5 rounded-full blur-[100px] pointer-events-none" />
         <div className="max-w-7xl mx-auto relative z-10">
           <Hero />
           <div className="bento-grid">
